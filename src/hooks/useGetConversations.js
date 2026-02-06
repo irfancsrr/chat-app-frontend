36e8368ffefbe8f939ab7303a3c298433ab1c643
irfan <irfancsrrsimt@gmail.com>
2026-02-06 18:27:18 +0530

    chat-app-frontend

---
+++ src/hooks/useGetConversations.js
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+import toast from "react-hot-toast";
+
+const useGetConversations = () => {
+	const [loading, setLoading] = useState(false);
+	const [conversations, setConversations] = useState([]);
+
+	useEffect(() => {
+		const getConversations = async () => {
+			setLoading(true);
+			try {
+				const res = await fetch("http://localhost:5000/api/users",{
+					method:"GET",
+					credentials:"include",
+				});
+				const data = await res.json();
+				if (data.error) {
+					throw new Error(data.error);
+				}
+				setConversations(data);
+			} catch (error) {
+				if(error.message=="Unauthorized - No Token  Provided"){
+					localStorage.removeItem("chat-user");
+					window.location.href='/login';
+				}
+				toast.error(error.message);
+			} finally {
+				setLoading(false);
+			}
+		};
+
+		getConversations();
+	}, []);
+
+	return { loading, conversations };
+};
+export default useGetConversations;
